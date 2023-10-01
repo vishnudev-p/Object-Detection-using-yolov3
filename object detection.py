6d5ee7182824be62aba0dfd195a61cdbc2cd7b4e
Vishnu Dev P <141658861+vishnudev-p@users.noreply.github.com>
2023-10-01 21:16:19 +0530

    Add files via upload

---
+++ object detection.py
@@ -0,0 +1,85 @@
+import cv2
+import numpy as np
+
+def get_output_layers(net):
+    layer_names = net.getLayerNames()
+    output_indices = net.getUnconnectedOutLayers()
+    
+    if output_indices.ndim == 2:
+        output_indices = output_indices[:, 0]
+
+    return [layer_names[i - 1] for i in output_indices]
+
+
+def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h, classes, COLORS):
+    label = str(classes[class_id])
+    color = COLORS[class_id]
+    cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), color, 2)
+    cv2.putText(img, label, (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+def main():
+    # Load YOLO
+    net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+    classes = []
+    with open("coco.names", "r") as f:
+        classes = [line.strip() for line in f.readlines()]
+
+    COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+
+    # Load image
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, image = cap.read()
+        Width = image.shape[1]
+        Height = image.shape[0]
+
+        # Detecting objects
+        blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+        net.setInput(blob)
+        outs = net.forward(get_output_layers(net))
+
+        class_ids = []
+        confidences = []
+        boxes = []
+
+        # Information about detected objects
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.5:
+                    # Object detected
+                    center_x = int(detection[0] * Width)
+                    center_y = int(detection[1] * Height)
+                    w = int(detection[2] * Width)
+                    h = int(detection[3] * Height)
+                    x = center_x - w / 2
+                    y = center_y - h / 2
+                    class_ids.append(class_id)
+                    confidences.append(float(confidence))
+                    boxes.append([x, y, w, h])
+
+        indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+        # Draw bounding boxes
+        for i in indices.flatten():
+
+            box = boxes[i]
+            x = box[0]
+            y = box[1]
+            w = box[2]
+            h = box[3]
+            draw_prediction(image, class_ids[i], confidences[i], round(x), round(y), round(x + w), round(y + h), classes, COLORS)
+
+        cv2.imshow("Object detection", image)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
